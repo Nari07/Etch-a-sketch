ac57a124ccb072fd5fb09653fffdb87517ebfb22
Nari07 <mickinguyen12@gmail.com>
2023-02-09 23:29:45 -0600

    fix border

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
 const gridContainer = document.getElementById('gridContainer');
 const cell = document.getElementById('cell');
 const eraser = document.getElementById('eraser');
-let pixel = document.getElementsByClassName('row');
-const sketch = document.getElementById('sketch');
+const pixel = document.getElementsByClassName('row');
+const ink = document.getElementById('ink');
 const slider = document.getElementById("myRange");
 const output = document.getElementById("size");
+const rainbow = document.getElementById("rainbow");
+const pencil = document.getElementById("pencil");
 
 let mouseDown = false;
 let eraseButton = false;
+let rgbButton = false;
+let pencilButton = false;
 let sliderValue = slider.value;
 
-eraser.addEventListener('click', function(){
-  eraseButton = true;
-  mouseDown = false;
-});
 
-sketch.addEventListener('click', function(){
-  eraseButton = false;
-  paintPixel();
-});
 
 // slider value
 output.innerHTML = slider.value + ' x ' + slider.value;
@@ -28,7 +24,7 @@ slider.addEventListener('input', function(){
 });
 
 
-
+//create new grid based on slider input
 slider.oninput = function(){
   document.querySelectorAll('.row').forEach(e => e.remove());
   createGrid(slider.value);
@@ -44,17 +40,12 @@ function createRow(val){
   for (i = 0; i < val; i++){
     let cellRow = document.createElement('div');
     cellRow.className = 'row';
-    cellRow.innerText = ' ';
     cellRow.id = 'cell';
     gridContainer.appendChild(cellRow);
 
     cellRow.addEventListener('mousedown', function(e){
       mouseDown = true;
-      if (eraseButton === true) {
-        e.target.style.backgroundColor = 'white';
-      } else {
-        e.target.style.backgroundColor = 'black';
-      }
+      paintPixel(e);
     });
 
     cellRow.addEventListener('mouseup', function(){
@@ -67,9 +58,10 @@ function createRow(val){
       eraseButton = false;
       for (let i = 0; i < pixel.length; i++){
         pixel[i].style.backgroundColor = 'white';
+       	pixel[i].style.border = '2px solid rgba(157, 174, 180, 0.2';
       }
      });
-
+		
   }
 
 }
@@ -80,20 +72,65 @@ function createGrid(val){
   }
 }
 
-
-createGrid(16);
+createGrid(20);
 gridContainer.style.gridTemplateColumns='repeat(' + `${sliderValue}` + ', [col-start] 25px)';
 
 
-function paintPixel(e){
+function paintPixel(e){  
   if (mouseDown === true){
     e.target.style.backgroundColor = ('black');
-  }
-  
-  if (eraseButton === true && mouseDown === true) {
+    e.target.style.margin = '0px';
+    e.target.style.margin = '0px';
+    e.target.style.border = '0px';
+    e.target.style.backgroundClip = 'border-box';
+  } 
+  if (eraseButton === true && mouseDown === true){
     e.target.style.backgroundColor = ('white');
+    e.target.style.backgroundClip = 'border-box';
+
+
+  }
+  if (rgbButton === true && mouseDown === true){
+    let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    e.target.style.backgroundColor = randomColor;
+    e.target.style.margin = '0px';
+    e.target.style.border = '0px';
+    e.target.style.backgroundClip = 'border-box';
+  }
+  if (pencilButton === true && mouseDown === true){
+  	e.target.style.backgroundColor = ('rgba(52,52,52, 0.15)');
+		e.target.style.margin = '0px';
+    e.target.style.border = '0px';
+    e.target.style.backgroundClip = 'border-box';
+
   }
 }
 
+eraser.addEventListener('click', function(){
+   eraseButton = true;
+   mouseDown = false;
+   rgbButton = false;
+   pencilButton = false;
+   paintPixel();
+});
 
+ink.addEventListener('click', function(){
+  eraseButton = false;
+  rgbButton = false;
+  pencilButton = false;
+  paintPixel();
+});
+
+rainbow.addEventListener('click', function(){
+  rgbButton = true;
+  pencilButton = false;
+  eraseButton = false;
+  paintPixel();
+});
+pencil.addEventListener('click', function(){
+  pencilButton = true;
+  rgbButton = false;
+  eraseButton = false;
+  paintPixel();
+});
 
